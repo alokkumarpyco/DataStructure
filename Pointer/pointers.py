75a274c90f48fa0366fbb2cae7d3475d266bdff6
alokkumarpyco <kumar.alok4241@gmail.com>
2023-03-15 18:12:55 +0900

    pointers

---
+++ Pointer/pointers.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = num1
+
+print("num1 value :", num1)
+print("num2 value :", num2)
+print()
+
+print("Memory address of num1 :", id(num1))
+print("Memory address of num2 :", id(num2))
